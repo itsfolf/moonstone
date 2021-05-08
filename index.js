92cc1498188aaf4f8ffe70d56f42793d2a8754ce
Checkium <checkium@gmail.com>
2021-05-08 17:54:02 +0100

    Better imports

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ function Moonstone(token, options) {
   return new Client(token, options);
 }
 
+Moonstone.Client = Client;
+Moonstone.User = require("./lib/core/User");
+Moonstone.Room = require("./lib/core/Room");
+Moonstone.ActiveUser = require("./lib/core/ActiveUser");
+Moonstone.ActiveRoom = require("./lib/core/ActiveRoom");
+Moonstone.Message = require("./lib/core/Message");
+Moonstone.Constants = require("./lib/Constants");
+Moonstone.VERSION = require("./package.json").version;
+
 module.exports = Moonstone;
